cef92a04c8a5d31374a997bae2bca8810c0771f1
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2020-11-04 04:10:48 +0300

    add servlet and filter

---
+++ src/main/java/ru/geebrains/CatalogServlet.java
@@ -0,0 +1,38 @@
+package ru.geebrains;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/catalog")
+public class CatalogServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(CatalogServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("<title>Catalog</title>");
+        response.getWriter().println("<h1>This is Catalog</h1>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/main'>Main Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/catalog'>Catalog Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/product'>Product Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/cart'>Cart Page</a></li>");
+        response.getWriter().println("<li><a href='" +
+                ""+request.getContextPath() +"/order'>Order Page</a></li>");
+        response.getWriter().println("</ul>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
+}
---
+++ src/main/java/ru/geebrains/ProductServlet.java
@@ -0,0 +1,37 @@
+package ru.geebrains;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(ProductServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("<title>Product Page</title>");
+        response.getWriter().println("<h1>This is Product Page</h1>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/main'>Main Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/catalog'>Catalog Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/product'>Product Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/cart'>Cart Page</a></li>");
+        response.getWriter().println("<li><a href='"+request.getContextPath() +"/order'>Order Page</a></li>");
+        response.getWriter().println("</ul>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
+}
